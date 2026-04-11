c77556a1820ff21f1f796ce232b9de760053844e
lasse33500 <lasse.syrjala@gmail.com>
2026-04-11 11:39:56 +0300

    fix mergesort formatting

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,19 +1,8 @@
-def debug_print(debug_msg=None, **kwargs):
-    if debug_msg:
-        print(debug_msg)
-
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
-
-
 def mergesort(array):
-    debug_print(array=array)
-
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
-    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -24,10 +13,6 @@ def mergesort(array):
 def merge(left, right):
     merged = []
 
-    debug_print("Merging...")
-    debug_print(left=left)
-    debug_print(right=right)
-
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
             merged.append(left.pop(0))
@@ -36,8 +21,6 @@ def merge(left, right):
 
     merged.extend(left)
     merged.extend(right)
-
-    debug_print(merged=merged)
 
     return merged
 
@@ -48,14 +31,10 @@ if __name__ == "__main__":
 
     value_list = []
     for x in input_list:
-        try:
-            value_list.append(int(x))
-        except ValueError:
-            print("Invalid input.")
-            exit(1)
+        value_list.append(int(x))
 
-    debug_print(input_list=input_list)
-    debug_print(value_list=value_list)
+    print("input_list:", input_list)
+    print("value_list:", value_list)
 
     sorted_list = mergesort(value_list)
-    print (mergesort(value_list))   
+    print(sorted_list)
