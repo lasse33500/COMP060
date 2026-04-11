37831eae92bf0dc981335bb13332145e73b8370b
lasse33500 <lasse.syrjala@gmail.com>
2026-04-11 11:21:36 +0300

    Implement debug mergesort

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,18 +1,19 @@
-
 def debug_print(debug_msg=None, **kwargs):
-
     if debug_msg:
         print(debug_msg)
 
     for key, value in kwargs.items():
-        print("{}: {}".format(key, value))
+        print(f"{key}: {value}")
 
 
 def mergesort(array):
+    debug_print(array=array)
+
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
+    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -23,16 +24,20 @@ def mergesort(array):
 def merge(left, right):
     merged = []
 
+    debug_print("Merging...")
+    debug_print(left=left)
+    debug_print(right=right)
+
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
             merged.append(left.pop(0))
         else:
             merged.append(right.pop(0))
 
-    if len(left) > 0:
-        merged += left
-    else:
-        merged += right
+    merged.extend(left)
+    merged.extend(right)
+
+    debug_print(merged=merged)
 
     return merged
 
@@ -40,13 +45,17 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
     input_list = input_str.split(",")
+
     value_list = []
     for x in input_list:
         try:
             value_list.append(int(x))
-        except ValueError as err:
+        except ValueError:
             print("Invalid input.")
-            quit(1)
+            exit(1)
+
+    debug_print("input_list", input_list=input_list)
+    debug_print("value_list", value_list=value_list)
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
